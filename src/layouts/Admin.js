f6f90d63f2a4396bfbf64eeaef84f39c8fa3677f
Omer Eckstein <omer.eckstein@shieldfc.com>
2022-09-22 23:36:12 +0300

    update admin page

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,42 +1,94 @@
 import React, { useState, useEffect } from 'react';
 import './Admin.css';
-import { Button } from '@mui/material';
+import { Button, Paper } from '@mui/material';
 import Card from '@mui/material/Card';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
 import API from '@aws-amplify/api';
 
 
 
 function App() {
-  // const [page, setPage] = useState(0);  
-  // const [admin, setAdmin] = useState(false);  
-
- 
-  async function getConfigure(){
-    // TODO
-    await API.get("apicogni", `/api/tests/clock/`)   
-  }
+  const [clockTitle, setClockTitle] = useState([]);  
+  const [clockLabels, setClockLabels] = useState([]);  
 
   useEffect(() => {
-    // getConfigure()
+    getConfigure()
   });
+
+  async function getConfigure(){
+    await getClockConfig(); 
+  }
+
+  function getClockConfig () {
+    API.get("apicogni", "/api/tests/clock/alltitles").then((res)=>{setClockTitle(res)})
+    API.get("apicogni", "/api/test/clock/labels").then((res)=>{setClockLabels(res)})
+  }
+
+  
+  function configTwoValues(value, title){
+    return(
+            <Stack direction="row">
+              <TextField className='config_textField'
+                hiddenLabel
+                id="filled-hidden-label-normal"
+                defaultValue={value}
+                variant="filled"
+              />
+              <div className='config_title'>{title}</div>
+            </Stack>      
+    );
+  }
+
   
   return (
     <Card className="card">
       <div width="100%">
+        <p>Shulman Clock</p>
+        <Paper className='paper_config' elevation={3} width="70%">
+          <Stack
+            component="form"
+            spacing={2}
+            noValidate
+            autoComplete="off"
+          >
+            {configTwoValues(clockTitle, "כותרת:")}
+            
+            
+            
+            <TextField
+              hiddenLabel
+              id="filled-hidden-label-normal"
+              defaultValue="Normal"
+              variant="filled"
+            />
+            <TextField
+              hiddenLabel
+              id="filled-hidden-label-normal"
+              defaultValue="Normal"
+              variant="filled"
+            />
+            <Button 
+              variant="contained"
+                size="medium" 
+                onClick={async () => {
+                  
+                }}
+          >
+                  עדכן 
+          </Button>
+          </Stack>
 
+
+        
+        
+          
+        </Paper>
         {/* tests configure */}
         {/* Clock img, title */}
         {/* SI - img, qs per lang */}
 
-        <Button 
-            variant="contained"
-              size="medium" 
-              onClick={async () => {
-                
-              }}
-        >
-                עדכן 
-        </Button>
+        
       </div>
     </Card>
   );
